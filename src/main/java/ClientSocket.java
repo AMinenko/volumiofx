c7390b81a1f8d27c6430af37e963aa6a1821d7c5
Andriy Minenko <Andriy_Minenko@epam.com>
2017-01-30 17:23:00 +0200

    initial commit

---
+++ src/main/java/ClientSocket.java
@@ -0,0 +1,77 @@
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.concurrent.CountDownLatch;
+import java.util.function.Consumer;
+
+import javax.websocket.ClientEndpoint;
+import javax.websocket.CloseReason;
+import javax.websocket.ContainerProvider;
+import javax.websocket.DeploymentException;
+import javax.websocket.OnClose;
+import javax.websocket.OnMessage;
+import javax.websocket.OnOpen;
+import javax.websocket.Session;
+import javax.websocket.WebSocketContainer;
+
+@ClientEndpoint
+public class ClientSocket {
+
+    private Consumer<String> consumer;
+    private ConnectionThread connectionThread = new ConnectionThread();
+    ClientSocket clientSocket = this;
+
+    public ClientSocket(Consumer<String> consumer) {
+        this.consumer = consumer;
+    }
+
+
+    CountDownLatch latch = new CountDownLatch(1);
+    private Session session;
+
+    public void startConnection() {
+        connectionThread.setDaemon(true);
+        connectionThread.setName("ClientSocketThread");
+        connectionThread.start();
+    }
+
+    @OnOpen
+    public void onOpen(Session session) {
+        System.out.println("Connected to server");
+        this.session = session;
+        latch.countDown();
+    }
+
+    @OnMessage
+    public void onText(String message, Session session) {
+        consumer.accept(message);
+        System.out.println("Message received from server:" + message);
+    }
+
+    @OnClose
+    public void onClose(CloseReason reason, Session session) {
+        System.out.println("Closing a WebSocket due to " + reason.getReasonPhrase());
+    }
+
+    public void sendMessage(String str) throws IOException {
+        session.getBasicRemote().sendText(str);
+    }
+
+    private class ConnectionThread extends Thread {
+
+        @Override
+        public void run() {
+            try {
+                WebSocketContainer container = ContainerProvider.getWebSocketContainer();
+                container.connectToServer(clientSocket, new URI("ws://192.168.1.201:3000/socket.io/?EIO=3&transport=websocket"));
+            } catch (IOException e) {
+                consumer.accept("Connection cosed");
+            } catch (DeploymentException e) {
+                e.printStackTrace();
+            } catch (URISyntaxException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
---
+++ src/main/java/StatusScreen.java
@@ -0,0 +1,50 @@
+import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+import javax.websocket.DeploymentException;
+import java.io.IOException;
+import java.net.URISyntaxException;
+
+public class StatusScreen extends Application {
+
+    private TextArea message = new TextArea();
+
+
+    private Parent createContent() throws URISyntaxException, IOException, DeploymentException {
+        ClientSocket client = new ClientSocket(data ->
+                Platform.runLater(() -> message.appendText(data)));
+        client.startConnection();
+
+        message.setPrefHeight(500.);
+        TextField input = new TextField();
+        input.setOnAction(event -> {
+            String inputText = input.getText();
+            try {
+                client.sendMessage(inputText);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        });
+
+        VBox root = new VBox(20, message, input);
+        root.setPrefSize(800, 600);
+        return root;
+    }
+
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        primaryStage.setScene(new Scene(createContent()));
+       // primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
